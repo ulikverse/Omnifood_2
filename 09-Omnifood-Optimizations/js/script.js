27042651a4ef3ea281f66631d55e0d9a43808c98
YULII SHTEL <ulik1shtel@gmail.com>
2021-12-28 12:40:48 +0200

    "Implementing Smooth Scrolling"

--- 09-Omnifood-Optimizations/js/script.js
+++ 09-Omnifood-Optimizations/js/script.js
@@ -1,6 +1,6 @@
 console.log("Hello world!");
 
-const myName = "Yulii Shtel";
+const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
@@ -12,48 +12,100 @@ console.log(h1);
 // });
 
 ///////////////////////////////////////////////////////////
-
-//Set current year
+// Set current year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 ///////////////////////////////////////////////////////////
+// Make mobile navigation work
 
-// Make Mobile navigation work
-
-const bntNavEl = document.querySelector(".btn-mobile-nav");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
 
-bntNavEl.addEventListener(
-  "click",
-  function () {
-    headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
   }
+);
+obs.observe(sectionHeroEl);
 
-  ///////////////////////////////////////////////////////////
-  // Fixing flexbox gap property missing in some Safari versions
-  // function checkFlexGap() {
-  //   var flex = document.createElement("div");
-  //   flex.style.display = "flex";
-  //   flex.style.flexDirection = "column";
-  //   flex.style.rowGap = "1px";
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-  //   flex.appendChild(document.createElement("div"));
-  //   flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
-  //   document.body.appendChild(flex);
-  //   var isSupported = flex.scrollHeight === 1;
-  //   flex.parentNode.removeChild(flex);
-  //   console.log(isSupported);
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
 
-  //   if (!isSupported) document.body.classList.add("no-flexbox-gap");
-  // }
-  // checkFlexGap();
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
 
-  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
-  /*
+/*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
   margin-right: 4.8rem;
 }
@@ -103,4 +155,3 @@ bntNavEl.addEventListener(
   }
 }
 */
-);
